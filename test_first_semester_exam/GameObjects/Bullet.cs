3f5f7968900fb9221996f0084473af57e38ee615
alex9505 <57255110+alex9505@users.noreply.github.com>
2019-11-30 23:33:12 +0100

    Created a bunch of classes
    Created a bunch of classes:
    - Bullet
    - Enemy
    - Player

---
+++ test_first_semester_exam/GameObjects/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_first_semester_exam
+{
+	class Bullet
+	{
+		private float moveSpeed;
+
+		public void Move()
+		{
+
+		}
+	}
+}
---
+++ test_first_semester_exam/GameObjects/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_first_semester_exam
+{
+	class Enemy
+	{
+		private float health;
+		private float moveSpeed;
+
+		public void Move()
+		{
+
+		}
+		
+		public void Shoot()
+		{
+
+		}
+
+		public void Die()
+		{
+
+		}
+	}
+}
